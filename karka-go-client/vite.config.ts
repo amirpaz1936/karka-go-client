d6d91950d14d9c34345c54e8e9b42d1a191d9739
amirpaz1936 <amirpaz1936@gmail.com>
2024-12-23 15:57:31 +0200

    add PWA

---
+++ karka-go-client/vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Automatically updates the service worker
+      devOptions: {
+        enabled: true, // Enable PWA during development
+      },
+      manifest: {
+        name: 'hayde problems',
+        short_name: 'Amir',
+        description: 'My awesome client',
+        theme_color: '#262a2e',
+        icons: [
+          {
+            "src": "/pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "any"
+          },
+          {
+            "src": "/pwa-maskable-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/pwa-maskable-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          }
+        ],
+      },
+    }),
+  ],
+})
